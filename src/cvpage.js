853ff92b24377c090cdb8d2bcd03538b5de3cb74
CHAPELLE Quentin <chapelle.quentin@gmail.com>
2017-10-31 20:09:46 +0100

    init modal

--- src/cvpage.js
+++ src/cvpage.js
@@ -25,8 +25,6 @@ $(document).ready(function() {
     $('.venobox').venobox();
 });
 
-
-
 function paramSlick() {
     if ($(window).width() < 640) {
         $('#carousel-work').slick({ "slidesToShow": 1, "slidesToScroll": 1 })
